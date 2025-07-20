fa813f8089f44d12044b8a5a5789c89febd3ec3c
NoomRT <rueangyotnoom@gmail.com>
2025-07-21 01:13:51 +0700

    refactor: from the solutions folder can also use .to_string() at the end of match {}

--- topics/pattern_match/exercises/src/lib.rs
+++ topics/pattern_match/exercises/src/lib.rs
@@ -1,11 +1,12 @@
 pub fn num_to_string(num: u32) -> String {
     match num {
-        0 => "zero".to_string(),
-        1 => "one".to_string(),
-        2 => "two".to_string(),
-        3 => "three".to_string(),
-        _ => "other".to_string(),
+        0 => "zero",
+        1 => "one",
+        2 => "two",
+        3 => "three",
+        _ => "other",
     }
+    .to_string()
 }
 
 pub fn unwrap_or_default(x: Option<u32>, v: u32) -> u32 {
