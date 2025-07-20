4c0e86c297526640af7f218c41639b686699011a
NoomRT <rueangyotnoom@gmail.com>
2025-07-21 00:05:57 +0700

    Exercise 1: Return the sum of all integers in the "nums" vector and Exercise 2: Return a vector of length "n", filled with the value "i"

--- topics/for_loop/exercises/src/lib.rs
+++ topics/for_loop/exercises/src/lib.rs
@@ -1,7 +1,19 @@
 pub fn sum(nums: Vec<i32>) -> i32 {
-    todo!();
+    let mut sum: i32 = 0;
+    if nums.len() != 0 {
+        for n in nums {
+            sum += n;
+        }
+    } else {
+        return 0;
+    }
+    sum
 }
 
 pub fn fill(i: u32, n: usize) -> Vec<u32> {
-    todo!();
+    let mut v = vec![];
+    for _ in 0..n {
+        v.push(i);
+    }
+    v
 }
